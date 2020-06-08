404f199aa1bcb5758ef20e982fcb153191bfb3fd
Lloyd <lloydwaldt@hotmail.com>
2020-06-08 16:59:13 +0200

    - first step of create workout layout made

--- app/src/main/java/com/example/fitnessapp_theexhibition/models/Workout.kt
+++ app/src/main/java/com/example/fitnessapp_theexhibition/models/Workout.kt
@@ -1,6 +1,5 @@
 package com.example.fitnessapp_theexhibition.models
 
 data class Workout(val name:String, val description:String, val exercises:ArrayList<WorkoutExercise>) {
-    //TODO add muscle groups list
 
 }
